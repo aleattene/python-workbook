77201be19b347e57e8aca844063e30b5de37e6ed
Alessandro Attene <attene.tomorrowdevs@gmail.com>
2021-03-17 10:13:36 +0100

    Added file 02_Greeting.py

---
+++ Chap_01/02_Greeting.py
@@ -0,0 +1,12 @@
+
+""" 
+Program that asks the USER to enter his/her NAME and then 
+shows a GREETING MESSAGE (using the name entered by the user).
+"""
+
+# Acquisition USER DATA
+name = input("Name? ")
+surname = input("Surname? ")
+
+# Show to video RESULTS
+print("HELLO " + surname + " " + name)
