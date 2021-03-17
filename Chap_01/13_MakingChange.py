6760f3434ed09c82886d9f0926d6e4e53f3d1581
Alessandro Attene <attene.tomorrowdevs@gmail.com>
2021-03-17 19:36:29 +0100

    Added file 13_MakingChange.py

---
+++ Chap_01/13_MakingChange.py
@@ -0,0 +1,53 @@
+"""
+The Program receives in INPUT from the USER a certain number
+of Cents of Dollar (Canadian) and displays the EXCHANGE
+with COINS as large as possible.
+"""
+
+# Declaration of VARIABLES
+toonies = 0     # 1 toonies = 200 cents = 2 $ CANADIANS
+loonies = 0     # 1 loonies = 100 cents = 1 $ CANADIAN
+quarters = 0    # 1 quarters = 25 cents = 1/4 di $ CANADIAN
+dimes = 0       # 1 dimes = 10 cents = 1/10 di $ CANADIAN
+nickels = 0     # 1 nickels = 5 cents = 1/20 di $ CANADIAN
+pennies = 0     # 1 pennies = 1 cent = 1/100 di $ CANADIAN
+
+
+# Acquisition and Control of the DATA entered by the USER
+cents = input("Enter the quantity (CENTS) to CHANGE: ")
+
+while not(cents.isdigit() and cents != "0"):
+    print("Incorrect entry. Try again.")
+    cents = input("Enter the quantity (CENTS) to CHANGE: ")
+
+
+# Conversion STR -> INT
+cents = int(cents)
+
+# Computing RESULTS (toonies, loonies, quarters, dimes, nickels, pennies)
+if cents >= 200:
+    toonies = int(cents / 200)
+    cents = cents % 200
+if cents >= 100:
+    loonies = int(cents / 100)
+    cents = cents % 100
+if cents >= 25:
+    quarters = int(cents / 25)
+    cents = cents % 25
+if cents >= 10:
+    dimes = int(cents / 10)
+    cents = cents % 10
+if cents >= 5:
+    nickels = int(cents / 5)
+    cents = cents % 5
+
+pennies = cents
+
+# Displaying the RESULTS
+print("RESULTS:")
+print("- Toonies = %i" % toonies)
+print("- Loonies = %i" % loonies)
+print("- Quarters = %i" % quarters)
+print("- Dimes = %i" % dimes)
+print("- Nickels = %i" % nickels)
+print("- Pennies = %i" % pennies)
