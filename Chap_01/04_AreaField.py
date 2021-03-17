7888af51ce8af60c63fb7c3da68b037fef3cea02
Alessandro Attene <74595044+aleattene@users.noreply.github.com>
2021-03-17 11:59:51 +0100

    Fixed comments (row 12 & row 15)

--- Chap_01/04_AreaField.py
+++ Chap_01/04_AreaField.py
@@ -9,8 +9,8 @@ The final result is then displayed.
 width = float(input("Enter the WIDTH of a FARMER'S FIELD (feet): "))
 length = float(input("Enter also the LENGTH (feet): "))
 
-# Computing FARMER'S FIELD (acres) -> (1 ACRE = 43.560 square feet)
+# Computing the FARMER'S FIELD AREA (in acres) -> (1 ACRE = 43.560 square feet)
 area = (width * length) / 43560
 
-# Displaying FINAL RESULT (Formatting AREA value with 3 decimal places after comma)
+# Displaying the FARMER'S FIELD AREA (three digital digits after the comma)
 print("The AREA of a farmer's field is " + "%.3f" % area + " acres")
