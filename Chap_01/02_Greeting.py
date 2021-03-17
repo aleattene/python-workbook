000efbdd94bc7c7ba5502bc804206d34e1a192cf
Alessandro Attene <74595044+aleattene@users.noreply.github.com>
2021-03-17 16:29:37 +0100

    Modified  "data" & "user" to "DATA" & "USER"

--- Chap_01/02_Greeting.py
+++ Chap_01/02_Greeting.py
@@ -4,7 +4,7 @@ Program that asks the USER to enter his/her NAME and then
 shows a GREETING MESSAGE (using the name entered by the user).
 """
 
-# Acquisition of data entered by the user
+# Acquisition of DATA entered by the USER
 name = input("Name? ")
 surname = input("Surname? ")
 
