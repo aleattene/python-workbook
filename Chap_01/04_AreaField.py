9b9abc59d50b6dcdeb862006618395969a64abf0
Alessandro Attene <attene.tomorrowdevs@gmail.com>
2021-03-17 10:46:06 +0100

    Added file 04_AreaField.py

---
+++ Chap_01/04_AreaField.py
@@ -0,0 +1,16 @@
+
+""" 
+Program that asks the USER to enter the SIZE of a FARMER'S FIELD 
+(width and length) to calculate its AREA in ACRES (1 ACRE = 43.560 square feet). 
+The final result is then displayed.
+"""
+
+# Acquisition USER DATA
+width = float(input("Enter the WIDTH of a FARMER'S FIELD (feet): "))
+length = float(input("Enter also the LENGTH (feet): "))
+
+# Calculation FARMER'S FIELD (acres) -> (1 ACRE = 43.560 square feet)
+area = (width * length) / 43560
+
+# Displaying FINAL RESULT (Formatting AREA value with 3 decimal places after comma)
+print("The AREA of a farmer's field is " + "%.3f" % area + " acres")
