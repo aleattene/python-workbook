c791fb9b30eeb86bb0a23e2acbc7351bf20d67ae
Alessandro Attene <74595044+aleattene@users.noreply.github.com>
2021-03-17 11:56:26 +0100

    Fixed comments (row 12 & row 15)

--- Chap_01/03_AreaRoom.py
+++ Chap_01/03_AreaRoom.py
@@ -9,8 +9,8 @@ The final result is then displayed.
 width = float(input("Enter the WIDTH of your room (meters): "))
 length = float(input("Enter also the LENGTH (meters): "))
 
-# Computing ROOM AREA (square meters)
+# Computing the ROOM AREA (square meters)
 area = width * length
 
-# Displaying FINAL RESULT
+# Displaying the ROOM AREA
 print("The AREA of your ROOM is " + str(area) + " square meters")
