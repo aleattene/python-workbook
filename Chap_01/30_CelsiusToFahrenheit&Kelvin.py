35fae413801c6a63d06c61a408ce9ff906534e63
Alessandro Attene <attene.tomorrowdevs@gmail.com>
2021-03-18 21:46:12 +0100

    Added file 30_CelsiusToFahrenheit&Kelvin.py

---
+++ Chap_01/30_CelsiusToFahrenheit&Kelvin.py
@@ -0,0 +1,81 @@
+"""
+The Program receives from the USER a temperature expressed in degrees Celsius
+and returns the same temperature expressed in both Kelvin and Fahrenheit degrees.
+"""
+
+# START Definition of FUNCTIONS
+
+
+def valutaFloat(numero):
+    countPoints = 0
+    for char in numero:
+        if ord(char) == 46:
+            countPoints += 1
+    if countPoints == 1 and numero != "." and valutaNumero(numero):
+        if isinstance(float(numero), float) and float(numero) > 0:
+            return True
+    else:
+        return False
+
+
+def valutaNumero(numero):
+    if numero == "":
+        return False
+    countSigns = 0
+    for char in numero:
+        if ord(char) == 45 or ord(char) == 43:
+            countSigns += 1
+    if ((numero[0] == "+") or (numero[0] == "-")) and countSigns == 1 and \
+            numero != "-" and numero != "+" and numero != "-." and numero != "+.":
+        return True
+    elif numero[0].isdigit() and countSigns == 0:
+        return True
+    else:
+        return False
+
+
+def validaEntry(temperatura):
+    if valutaNumero(temperatura) or valutaFloat(temperatura):
+        return True
+    return False
+
+
+def celsiusFahrenheit(degreesCelsius):
+    degreesFahrenheit = (degreesCelsius * 9 / 5) + 32
+    return degreesFahrenheit
+
+
+def celsiusKelvin(degreesCelsius):
+    degreesKelvin = degreesCelsius + 273.15
+    return degreesKelvin
+
+
+# END Definition of FUNCTIONS
+
+
+# Acquisition and Control of the DATA entered by the USER
+degreesCelsius = input("Enter the TEMPERATURE (degrees Celsius): ")
+
+degreesCelsiusValidated = validaEntry(degreesCelsius)
+
+while not(degreesCelsiusValidated):
+    print("Incorrect entry. Try again.")
+    degreesCelsius = input("Enter the TEMPERATURE (degrees Celsius): ")
+    degreesCelsiusValidated = validaEntry(degreesCelsius)
+
+
+# Conversion STR -> FLOAT
+degreesCelsius = float(degreesCelsius)
+
+
+# Conversion Celsius -> Fahrenheit && Celsius -> Kelvin
+degreesFahrenheit = celsiusFahrenheit(degreesCelsius)
+degreesKelvin = celsiusKelvin(degreesCelsius)
+
+
+# Displaying the RESULTS
+print("RESULTS -> %.2f" % degreesCelsius + " °C = %.2f" % degreesFahrenheit +
+      " °F alias %.2f" % degreesKelvin + " °K")
+
+
+# Possible evolution -> check that the values are included in the respective scales
