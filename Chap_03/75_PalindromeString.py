eae8b55523a12b38f2a17295a3bd821a19165f98
Alessandro Attene <attene.tomorrowdevs@gmail.com>
2021-03-25 22:29:49 +0100

    Added file 75_PalindromeString.py

---
+++ Chap_03/75_PalindromeString.py
@@ -0,0 +1,36 @@
+"""
+The program receives from the USER a STRING
+and returns (displaying it) whether or not it is a PALINDROME.
+"""
+
+
+# START Definition of FUNCTION
+
+def checkWord(string):
+    string = string.upper()
+    index_sx = 0                    # left index
+    index_dx = len(string)-1        # right index
+    while index_sx < index_dx:
+        if string[index_sx] != string[index_dx]:
+            return "IS NOT a PALINDROME."
+        index_sx += 1               # increasing left index
+        index_dx -= 1               # decreasing right index
+    return "IS a PALINDROME."
+
+
+# END Definition of FUNCTION
+
+
+# Acquisition and Control of the DATA entered by the USER
+word = input("Enter the STRING: ")
+
+
+# Evaluation if STRING is a PALINDROME
+palindrome = checkWord(word)
+
+
+# Displaying the RESULT
+print("\"" + word.upper() + "\" " + palindrome)
+
+
+# Possible evolution -> check input utente = characters and not symbols 
