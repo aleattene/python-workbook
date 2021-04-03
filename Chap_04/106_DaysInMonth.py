b10da676907b36a2e6578795b3ba8f58b98509e1
Alessandro Attene <attene.tomorrowdevs@gmail.com>
2021-04-03 14:32:51 +0200

    Added file 106_DaysInMonth.py

---
+++ Chap_04/106_DaysInMonth.py
@@ -0,0 +1,79 @@
+"""
+The program receives a MONTH (1-12) and YEAR (four digits) from the USER
+and returns (displaying it) the NUMBER of DAYS (28-31) in that MONTH.
+"""
+
+# START Definition of FUNCTIONS
+
+
+def valutaYear(numero):                 # Possible evolution -> IMPORT
+    if numero.isdigit() and len(numero) == 4:
+        if int(numero) > 0:
+            return True
+    return False
+
+
+def valutaLeapYear(year):               # Possible evolution -> IMPORT
+    if year.isdigit():
+        year = int(year)
+        if year % 400 == 0 or (year % 4 == 0 and year % 100 != 0):
+            return True
+        else:
+            return False
+
+
+def validaMonth(month):                 # Possible evolution -> IMPORT
+    if month.isdigit():
+        if 1 <= int(month) <= 12:
+            return True
+    return False
+
+
+def daysInMonth(month, year):
+    month = int(month)
+    if ((month == 1) or (month == 3) or (month == 5) or (month == 7) or
+            (month == 8) or (month == 10) or (month == 12)):
+        return "31"
+    elif ((month == 4) or (month == 6) or (month == 9) or (month == 11)):
+        return "30"
+    elif (month == 2):
+        if valutaLeapYear(year):
+            return "29"
+        elif not(valutaLeapYear(year)):
+            return "28"
+
+
+def monthDescription(month):
+    months = ["JANUARY", "FEBRUARY", "MARCH", "APRIL", "MAY", "JUNE", "JULY",
+              "AUGUST", "SEPTEMBER", "OCTOBER", "NOVEMBER", "DECEMBER"]
+    return months[int(month)-1]
+
+
+# END Definition of FUNCTIONS
+
+
+# START MAIN PROGRAM
+def main():
+
+    # Acquisition and Control of the DATA entered by the USER
+    print("Enter a MONTH and a YEAR.")
+    month = input("MONTH (1-12): ")
+    year = input("YEAR (four digits): ")
+    monthValidated = validaMonth(month)
+    yearValidated = valutaYear(year)
+
+    while not(monthValidated and yearValidated):
+        print("Incorrect entry. Try again.")
+        print("Enter a MONTH and a YEAR.")
+        month = input("MONTH (1-12): ")
+        year = input("YEAR (four digits): ")
+        monthValidated = validaMonth(month)
+        yearValidated = valutaYear(year)
+
+    # Displaying the RESULTS
+    print("In {} {} there are {} DAYS.".format(
+        monthDescription(month), year, daysInMonth(month, year)))
+
+
+if __name__ == "__main__":
+    main()
