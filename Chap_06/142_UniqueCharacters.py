b9cfe4e68fabaa7061653e658298c23bdaff34f8
Alessandro Attene <attene.tomorrowdevs@gmail.com>
2021-04-15 15:16:57 +0200

    Added file 142_UniqueCharacters.py

---
+++ Chap_06/142_UniqueCharacters.py
@@ -0,0 +1,55 @@
+"""
+The program receives a TEXT MESSAGE from the USER
+and returns (displaying it) the NUMBER of ITS UNIQUE CHARACTERS.
+"""
+
+
+# START Definition of the FUNCTION
+
+
+def charsOccurrences(text_message):
+    # DICTIONARY - Occurrences UNIQUE CHARACTERS
+    chars_occurrences = {}
+    # Analysis of the SINGLE CHAR of the TEXT MESSAGE
+    for char in text_message:
+        # Char already present in the dictionary
+        if char in chars_occurrences:
+            chars_occurrences[char] += 1
+        # Char not present in the dictionary
+        else:
+            chars_occurrences[char] = 1
+    return chars_occurrences
+
+
+# END Definition of FUNCTION
+
+
+# START MAIN PROGRAM
+def main():
+
+    # Acquisition of DATA entered by the USER
+    text_message = input("Enter the TEXT MESSAGE: ")
+    # STRING TESTING
+    # text_message = "Hello, World!"
+    # text_message = "zzz"
+
+    # COUNT of UNIQUE CHARS in the TEXT MESSAGE
+    unique_chars_dict = charsOccurrences(text_message)
+    unique_chars = len(unique_chars_dict)
+
+    # Displaying the RESULT
+    if unique_chars == 0:   # entered an empty message
+        print("The TEXT MESSAGE entered by the USER does NOT CONTAIN characters.")
+    else:
+        if unique_chars == 1:  # entered only one unique character in the message
+            result = "CONTAIN"
+            char_chars = "character."
+        else:
+            result = "CONTAIN"
+            char_chars = "characters."
+        print("The TEXT MESSAGE entered by the USER {} {} {}".format(
+            result, unique_chars, char_chars))
+
+
+if __name__ == "__main__":
+    main()
