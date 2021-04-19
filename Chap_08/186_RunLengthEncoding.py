1b1f97e1dd176b915929b03c8960ac7cb7ab3b73
Alessandro Attene <attene.tomorrowdevs@gmail.com>
2021-04-19 21:51:33 +0200

    Added file 186_RunLengthEncoding.py

---
+++ Chap_08/186_RunLengthEncoding.py
@@ -0,0 +1,55 @@
+"""
+The program starting from a DECODED LIST
+returns the same LIST ENCODED.
+"""
+
+
+# START Definition of FUNCTION
+
+
+def encodeList(list_to_encode):
+    # BASE CASE
+    if list_to_encode == []:
+        return []
+    elif len(list_to_encode) == 1:
+        return [list_to_encode[0], 1]
+    # RECURSIVE CASES
+    else:
+        tmp = []
+        tmp.append(list_to_encode[0])
+        tmp.append(1)
+        i = 1
+        while list_to_encode[i] == tmp[0]:
+            tmp[1] += 1
+            i += 1
+            if i == (len(list_to_encode)):
+                break
+        return tmp + encodeList(list_to_encode[i:len(list_to_encode)])
+
+
+# END Definition of FUNCTION
+
+
+# START MAIN PROGRAM
+def main():
+
+    # LIST TESTING
+    all_list_to_encode = [
+        ["A", "A", "A", "A", "A", "A", "B", "B",
+            "B", "B", "B", "C", "C", "C", "C", "D"],
+        ["A", "A", "A", "B", "B", "B", "C", "C", "C", "D", "D"],
+        ["A", "A", "A", "C", "C", "C"],
+        ["A", "C", "C", "C"],
+        ["A"]
+    ]
+
+    # Computing and displaying the RESULTS
+    for list_to_encode in all_list_to_encode:
+        print("ORIGINAL DECODED LIST -> ", end="")
+        print(list_to_encode)
+        print("ENCODED LIST -> ", end="")
+        print(encodeList(list_to_encode))
+
+
+if __name__ == "__main__":
+    main()
