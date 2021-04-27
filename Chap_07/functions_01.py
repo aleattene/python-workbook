d6199eb70be6453361974b85a12ba5ff8f9413b3
Alessandro Attene <attene.tomorrowdevs@gmail.com>
2021-04-27 20:23:51 +0200

    Added file functions_01.py

---
+++ Chap_07/functions_01.py
@@ -0,0 +1,15 @@
+# Commento
+def function_one():
+    return True
+
+
+def function_two():
+    return True
+
+    def function_three():
+        return True
+
+
+# Commento
+def function_four():
+    return True
