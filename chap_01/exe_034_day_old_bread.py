13928c592d2dff049acd753f2dd88898affdad01
Alessandro Attene <74595044+aleattene@users.noreply.github.com>
2021-10-23 21:57:47 +0200

    refactor: exe_034 - improved description

--- chap_01/exe_034_day_old_bread.py
+++ chap_01/exe_034_day_old_bread.py
@@ -1,12 +1,16 @@
 """
-The Program receives from the USER the NUMBER of LOAVES of DAY OLD BREAD
-and calculates the PURCHASE COST, considering that:
-- LOAVE of BREAD cost = $ 3.49
-- Discount LOAVE of DAY OLD BREAD = 60%
-After that, the program displays the RECEIPT with:
-- TOTAL COST of LOAVES of BREAD
-- DISCOUNT Applied
-- TOTAL PURCHASE COST
+A bakery sells loaves of bread for €3.49 each.
+Day old bread is discounted by 60 percent.
+Write a program that begins by reading
+the number of loaves of day old bread being purchased from the user.
+Then your program should display:
+- the regular price for the bread,
+- the discount because it is a day old, and
+- the total price.
+Each of these amounts should be displayed on its own line with an appropriate label.
+All of the values should be displayed using two decimal places, and
+the decimal points in all of the numbers should be aligned
+when reasonable values are entered by the user.
 """
 
 # START Definition of FUNCTIONS
@@ -36,13 +40,14 @@ def discount(cost, percDiscount):
 loavesBreadDO = input("Enter the NUMBER of DAY OLD LOAVES of BREAD: ")
 loavesBreadDOValidated = valutaIntPositive(loavesBreadDO)
 
+
 while not(loavesBreadDOValidated):
     print("Incorrect entry. Try again.")
     loavesBreadDO = input("Enter the NUMBER of DAY OLD LOAVES of BREAD: ")
     loavesBreadDOValidated = valutaIntPositive(loavesBreadDO)
 
 
-# Connversion STR -> INT
+# Conversion STR -> INT
 loavesBreadDO = int(loavesBreadDO)
 
 
