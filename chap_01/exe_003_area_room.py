32eca30f8b4bddbb14c20473fbbfebf410976f3a
Alessandro Attene <74595044+aleattene@users.noreply.github.com>
2021-10-23 19:22:02 +0200

    refactor: exe_003 - improved description and string formatting

--- chap_01/exe_003_area_room.py
+++ chap_01/exe_003_area_room.py
@@ -1,9 +1,12 @@
 
 """ 
-Program that asks the USER to enter the SIZE of a ROOM 
-(width and length) to calculate its AREA. 
-The final result is then displayed.
+Write a program that asks the user to enter the width and length of a room.
+Once these values have been read, your program should compute and display the area of the room.
+The length and the width will be entered as floating-point numbers.
+Include units in your prompt and output message; either feet or meters,
+depending on which unit you are more comfortable working with.
 """
+
 # Acquisition of DATA entered by the USER
 width = float(input("Enter the WIDTH of your room (meters): "))
 length = float(input("Enter also the LENGTH (meters): "))
@@ -12,4 +15,4 @@ length = float(input("Enter also the LENGTH (meters): "))
 area = width * length
 
 # Displaying the ROOM AREA
-print("The AREA of your ROOM is " + str(area) + " square meters")
+print(f'The AREA of your ROOM is {area} square meters.')
