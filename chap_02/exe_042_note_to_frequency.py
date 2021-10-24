915609ed7f9239ffaf1cd7f3a23a739895b86daa
Alessandro Attene <74595044+aleattene@users.noreply.github.com>
2021-10-24 14:35:00 +0200

    refactor: exe_042 - improved description

--- chap_02/exe_042_note_to_frequency.py
+++ chap_02/exe_042_note_to_frequency.py
@@ -1,6 +1,29 @@
 """
-The Program receives from the USER the NAME of a MUSICAL note (for example C1, B2, etc) 
-and displaying its frequency.
+The following table lists an octave of music notes,
+beginning with middle C, along with their frequencies.
+
+Note    Frequency (Hz)
+ C4	        261.63
+ D4	        293.66
+ E4	        329.63
+ F4	        349.23
+ G4	        392.00
+ A4	        440.00
+ B4	        493.88
+
+Begin by writing a program that
+reads the name of a note from the user and displays the note’s frequency.
+Your program should support all of the notes listed previously.
+
+Once you have your program working correctly for the notes listed previously
+you should add support for all of the notes from C0 to C8.
+While this could be done by adding many additional cases to your if statement,
+such a solution is cumbersome, inelegant and unacceptable for the purposes of this exercise.
+Instead, you should exploit the relationship between notes in adjacent octaves.
+In particular, the frequency of any note in octave n
+is half the frequency of the corresponding note in octave n + 1.
+By using this relationship, you should be able to add support
+for the additional notes without adding additional cases to your if statement.
 """
 
 # START Definition of FUNCTIONS
