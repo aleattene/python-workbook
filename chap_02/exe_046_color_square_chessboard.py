e57008d75cb94016da4082999705cba897015ae9
Alessandro Attene <74595044+aleattene@users.noreply.github.com>
2021-10-24 14:37:21 +0200

    refactor: exe_046 - improved description

--- chap_02/exe_046_color_square_chessboard.py
+++ chap_02/exe_046_color_square_chessboard.py
@@ -1,6 +1,15 @@
-""" 
-The program receives from the USER one POSITION of the CHESSBOARD
-and displays the COLOR of that POSITION (BLACK or WHITE)
+"""
+Positions on a chess board are identified by a letter and a number.
+The letter identifies the column, while the number identifies the row.
+
+Write a program that reads a position from the user.
+Use an if statement to determine if the column begins with a black square or a white square.
+Then use modular arithmetic to report the color of the square in that row.
+
+For example, if the user enters a1 then your program should report that the square is black.
+If the user enters d5 then your program should report that the square is white.
+Your program may assume that a valid position will always be entered.
+It does not need to perform any error checking.
 """
 
 # START Definition of FUNCTIONS
