0b4bb83a8cdf8eeca851accb8e424ac3cb912e3d
Alessandro Attene <74595044+aleattene@users.noreply.github.com>
2021-10-25 12:42:29 +0200

    style: exe_067 - improved description

--- chap_03/exe_067_polygon_perimeter.py
+++ chap_03/exe_067_polygon_perimeter.py
@@ -1,6 +1,24 @@
 """
-The program receives from the USER the COORDINATES (x,y) of the POINTS of a POLYGON
-and subsequently at inserting of an empty line calculates its PERIMETER.
+Write a program that computes the perimeter of a polygon.
+Begin by reading the x and y coordinates
+for the first point on the perimeter of the polygon from the user.
+Then continue reading pairs of values
+until the user enters a blank line for the x-coordinate.
+Each time you read an additional coordinate
+you should compute the distance to the previous point and add it to the perimeter.
+When a blank line is entered for the x-coordinate
+your program should add the distance from the last point back to the first point to the perimeter.
+Then the perimeter should be displayed.
+Sample input and output values are shown below.
+The input values entered by the user are shown in bold.
+
+Example:
+Enter the first x-coordinate: 0
+Enter the first y-coordinate: 0
+Enter the next x-coordinate (blank to quit): 1 Enter the next y-coordinate: 0
+Enter the next x-coordinate (blank to quit): 0 Enter the next y-coordinate: 1
+Enter the next x-coordinate (blank to quit):
+The perimeter of that polygon is 3.414213562373095
 """
 
 # IMPORT module MATH
