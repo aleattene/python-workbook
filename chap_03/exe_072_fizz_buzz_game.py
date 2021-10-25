58b11e109485e9fda18279cbaedef9d130211eaf
Alessandro Attene <74595044+aleattene@users.noreply.github.com>
2021-10-25 14:45:19 +0200

    style: exe_072 - improved description

--- chap_03/exe_072_fizz_buzz_game.py
+++ chap_03/exe_072_fizz_buzz_game.py
@@ -1,6 +1,23 @@
 """
-The program DISPLAYS the ANSWERS for the FIRST 100 NUMBERS
-in the FIZZ-BUZZ game.
+Fizz-Buzz is a game that is sometimes played by children
+to help them learn about division.
+The players are commonly arranged in a circle so that the game can progress
+from player to player continually.
+The starting player begins by saying one, and then play passes to the player to the left.
+Each subsequent player is responsible
+for the next integer in sequence before play passes to the following player.
+
+On a player’s turn they must either say their number or one of following substitutions:
+- if the player’s number is divisible by 3 then the player says fizz instead of their number,
+- if the player’s number is divisible by 5 then the player says buzz instead of their number.
+
+A player must say both fizz and buzz for numbers that are divisible by both 3 and 5.
+Any player that fails to perform the correct substitution or
+hesitates before answering is eliminated from the game.
+The last player remaining is the winner.
+
+Write a program that displays the answers for the first 100 numbers in the Fizz- Buzz game.
+Each answer should be displayed on its own line.
 """
 
 
@@ -16,8 +33,7 @@ print("|   of the FIZZ-BUZZ game   |")
 print("*****************************")
 
 
-# Computing the ANSWERS for the FIRST 100 NUMBERS (FIZZ-BUZZ game)
-in the FIZZ-BUZZ game.
+# Computing the ANSWERS for the FIRST 100 NUMBERS (FIZZ-BUZZ game) in the FIZZ-BUZZ game.
 while number <= 100:
     if number % 3 == 0:                             # divisible by 3
         if number % 5 == 0:                         # divisible (by 3) and by 5
