42bdff619a42eaffad938baad7dec4afe7766864
Alessandro Attene <74595044+aleattene@users.noreply.github.com>
2021-10-26 14:18:04 +0200

    style: exe_100 - improved description

--- chap_04/exe_100_random_password.py
+++ chap_04/exe_100_random_password.py
@@ -1,8 +1,10 @@
 """
-The program GENERATES and DISPLAYS a RANDOM PASSWORD:
-- having a random length between 7 and 10 characters
-- with each character randomly selected 
-    between position 33 and 126 of the ASCII table.
+Write a function that generates a random password.
+The password should have a random length of between 7 and 10 characters.
+Each character should be randomly selected from positions 33 to 126 in the ASCII table.
+Your function will not take any parameters.
+It will return the randomly generated password as its only result.
+Display the randomly generated password in your file’s main program.
 """
 
 # IMPORT module RANDOM
