a5f2dca741342147d638634d8c93ce08d884c5a1
Alessandro Attene <74595044+aleattene@users.noreply.github.com>
2021-10-26 14:22:06 +0200

    style: exe_104 - improved description

--- chap_04/exe_104_hexa_decimal_digits.py
+++ chap_04/exe_104_hexa_decimal_digits.py
@@ -1,6 +1,13 @@
 """
-The program receives from the USER a NUMBER in DECIMAL and/or HEXADECIMAL format
-and returns it (displaying it) CONVERTED  in hexadecimal and/or decimal format.
+Write two functions, hex2int and int2hex, that convert between
+hexadecimal digits (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, A, B, C, D, E and F) and
+decimal (base 10) integers.
+The hex2int function is responsible for converting a string containing a single hexadecimal digit to a base 10 integer,
+while the int2hex function is responsible for converting an integer between 0 and 15 to a single hexadecimal digit.
+Each function will take the value to convert as its only parameter and return the converted value as its only result.
+Ensure that the hex2int function works correctly for both uppercase and lowercase letters.
+Your functions should display a meaningful error message and end the program
+if the parameter’s value is outside of the expected range.
 """
 
 
