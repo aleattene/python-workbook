105e1b85a42c9cd38c156bb983269c7b8a02f176
Alessandro Attene <74595044+aleattene@users.noreply.github.com>
2021-10-26 13:07:19 +0200

    style: exe_090 - improved description

--- chap_04/exe_090_twelve_days_christmas_song.py
+++ chap_04/exe_090_twelve_days_christmas_song.py
@@ -1,7 +1,28 @@
 """
-The program receives from the USER an INTEGER number from 1 to 12
-and displays the song TWELVE DAYS OF CHRISTMAS
-up to the VERSE corresponding to the NUMBER INSERTED.
+The Twelve Days of Christmas is a repetitive song that
+describes an increasingly long list of gifts sent to one’s true love on each of 12 days.
+A single gift is sent on the first day.
+A new gift is added to the collection on each additional day, and then the complete collection is sent.
+The first three verses of the song are shown below.
+The complete lyrics are available on the Internet.
+
+On the first day of Christmas my true love sent to me: A partridge in a pear tree.
+
+On the second day of Christmas my true love sent to me: Two turtle doves, And a partridge in a pear tree.
+
+On the third day of Christmas my true love sent to me:
+Three French hens, Two turtle doves, And a partridge in a pear tree.
+
+Write a program that displays the complete lyrics for The Twelve Days of Christmas.
+Your program should include a function that displays one verse of the song.
+It will take the verse number as its only parameter.
+Then your program should call this function 12 times with integers that increase from 1 to 12.
+
+Each item that is sent to the recipient in the song should only appear in your program once,
+with the possible exception of the partridge.
+It may appear twice if that helps you handle the difference between “A partridge in a pear tree” in the first verse and
+“And a partridge in a pear tree” in the subsequent verses.
+You can even import your solution to Exercise 089 to help you complete this exercise.
 """
 
 # START Definition of the FUNCTIONS
