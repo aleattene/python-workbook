504eda6e3bc77181aba9b9104c7c3fdc5ee6671f
Alessandro Attene <74595044+aleattene@users.noreply.github.com>
2021-10-26 18:16:21 +0200

    style: exe_106 - improved description

--- chap_04/exe_106_days_in_month.py
+++ chap_04/exe_106_days_in_month.py
@@ -1,6 +1,10 @@
 """
-The program receives a MONTH (1-12) and YEAR (four digits) from the USER
-and returns (displaying it) the NUMBER of DAYS (28-31) in that MONTH.
+Write a function that determines how many days there are in a particular month.
+Your function will take two parameters:
+- the month as an integer between 1 and 12, and
+- the year as a four digit integer.
+Ensure that your function reports the correct number of days in February for leap years.
+Include a main program that reads a month and year from the user and displays the number of days in that month.
 """
 
 # START Definition of FUNCTIONS
