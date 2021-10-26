c41c2b82ae181909591b9db21e6948def9b11e4b
Alessandro Attene <74595044+aleattene@users.noreply.github.com>
2021-10-26 13:11:50 +0200

    style: exe_093 - improved description

--- chap_04/exe_093_centered_string.py
+++ chap_04/exe_093_centered_string.py
@@ -1,10 +1,16 @@
 """
-The program receives a STRING and the WIDTH of the terminal WINDOW (in CHARACTERS) from the USER
-and returns (displaying it) the STRING FORMATTED in the following manner:
-- if the length of the string is greater than or equal to the width of the terminal window
-    then only the string should be returned
-- otherwise, the string must be returned with the addition of
-    ((width - length(string)) // 2) spaces at precede and follow it
+Write a function that takes a string, s, as its first parameter, and
+the width of the window in characters, w, as its second parameter.
+Your function will return a new string that includes
+whatever leading spaces are needed
+so that s will be centered in the window when the new string is printed.
+
+The new string should be constructed in the following manner:
+- if the length of s is greater than or equal to the width of the window then s should be returned
+- if the length of s is less than the width of the window
+    then a string containing (len(s) - w) // 2 spaces followed by s should be returned.
+
+Write a main program that demonstrates your function by displaying multiple strings centered in the window.
 """
 
 
