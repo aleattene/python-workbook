1facc62427199b029550c66830622d9e953c94a5
Alessandro Attene <74595044+aleattene@users.noreply.github.com>
2021-10-26 12:25:50 +0200

    refactor: directory renamed from Chap_04 to chap_04 (and its files) according to the standard PEP

--- chap_04/exe_086_taxi_fare.py
+++ chap_04/exe_086_taxi_fare.py
@@ -1,10 +1,9 @@
 """
-The program receives from the USER
-the DISTANCE TRAVELLED (in kilometers)
-and returns (displaying it) the TAXI FARE,
-considering that:
-- BASE fare = $4.00
-- VARIABLE fare = $0.25 every 140 meters travelled.
+In a particular jurisdiction, taxi fares consist of a base fare of €4.00,
+plus €0.25 for every 140 meters travelled.
+Write a function that takes the distance travelled (in kilometers) as its only parameter and
+returns the total fare as its only result.
+Write a main program that demonstrates the function.
 """
 
 
