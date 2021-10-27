535a2f5cf5c6585ff9b02abdb5f881029efab02d
Alessandro Attene <74595044+aleattene@users.noreply.github.com>
2021-10-27 11:02:57 +0200

    style: exe_110 - improved description

--- chap_05/exe_110_sorted_list.py
+++ chap_05/exe_110_sorted_list.py
@@ -1,8 +1,9 @@
 """
-The program receives INTEGERS from the USER and stores them in a LIST. 
-The insertion continues until the user inserts ZERO.
-After that, the program displays ALL of the VALUES entered by the user 
-(except for the zero) in ASCENDING ORDER (with one value on each line).
+Write a program that reads integers from the user and stores them in an array.
+Your program should continue reading values until the user enters 0.
+Then it should display all of the values entered by the user (except for the 0) in ascending order,
+with one value appearing on each line.
+Use either the sort method or the sorted function to sort the array.
 """
 
 
