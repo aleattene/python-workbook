ac5e207b5bf60dea06f19fa47e61cbb44ae93955
Alessandro Attene <74595044+aleattene@users.noreply.github.com>
2021-10-27 11:06:39 +0200

    style: exe_113 - improved description

--- chap_05/exe_113_avoiding_duplicates.py
+++ chap_05/exe_113_avoiding_duplicates.py
@@ -1,7 +1,15 @@
 """
-The program receives WORDS from the USER UNTIL the user inserts a BLANK LINE.
-After that, the program displays EACH WORD entered by the USER 
-EXACTLY ONCE, in the SAME ORDER that they were first ENTERED.
+In this exercise, you will create a program that reads words from the user until the user enters a blank line.
+After the user enters a blank line your program should display each word entered by the user exactly once.
+The words should be displayed in the same order that they were first entered.
+
+For example, if the user enters:
+
+first second first third second
+
+then your program should display:
+
+first second third
 """
 
 # START MAIN PROGRAM
