ef7932c8ac8d081ea129f87e8caf238a20f4c9da
Alessandro Attene <74595044+aleattene@users.noreply.github.com>
2021-10-27 12:11:06 +0200

    style: exe_133 - improved description

--- chap_05/exe_133_sublist_of_list.py
+++ chap_05/exe_133_sublist_of_list.py
@@ -1,11 +1,23 @@
 """
-The program receives TWO LISTS OF INTEGERS from the USER
-and returns whether or not the SECOND LIST is a SUBLIST of the FIRST,
-also considering that:
-- an EMPTY list IS a SUBLIST of ANY other list 
-    (for example [] is a sublist of [1,2,3,4])
-- ANY LIST IS a SUBLIST of ITSELF 
-    (for example [1,2,3,4] is a sublist of [1,2,3,4])
+A sublist is a list that is part of a larger list.
+A sublist may be a list containing a single element, multiple elements, or even no elements at all.
+
+For example:
+[1], [2], [3] and [4] are all sublists of [1, 2, 3, 4].
+
+The list [2, 3] is also a sublist of [1, 2, 3, 4], but [2, 4] is not a sublist [1, 2, 3, 4]
+because the elements 2 and 4 are not adjacent in the longer list.
+
+The empty list is a sublist of any list.
+As a result, [] is a sublist of [1, 2, 3, 4].
+
+A list is a sublist of itself, meaning that [1, 2, 3, 4] is also a sublist of [1, 2, 3, 4].
+
+In this exercise you will create a function, isSublist,
+that determines whether or not one list is a sublist of another.
+Your function should take two lists, larger and smaller, as its only parameters.
+It should return True if and only if smaller is a sublist of larger.
+Write a main program that demonstrates your function.
 """
 
 
