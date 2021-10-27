5850f03012f07e1f559b15adb05da0c62685841e
Alessandro Attene <74595044+aleattene@users.noreply.github.com>
2021-10-27 11:22:35 +0200

    style: exe_116 - improved description

--- chap_05/exe_116_perfect_numbers.py
+++ chap_05/exe_116_perfect_numbers.py
@@ -1,9 +1,16 @@
 """
-The program displays ALL the PERFECT NUMBERS between 1 to 10.000.
-A number is defined PERFECT when the SUM of his own PROPER DIVISORS
-is EQUAL at the NUMBER itself.
-For example, 28 is a perfect number, because his own proper divisors are 1, 2, 4, 7 and 14
-and their sum is equal to 28.
+An integer, n, is said to be perfect when the sum of all of the proper divisors of n is equal to n.
+
+For example:
+28 is a perfect number because its proper divisors are 1, 2, 4, 7 and 14, and 1 + 2 + 4 + 7 + 14 = 28.
+
+Write a function that determines whether or not a positive integer is perfect.
+Your function will take one parameter:
+- if that parameter is a perfect number then your function will return True,
+- otherwise it will return False.
+
+In addition, write a main program that uses your function
+to identify and display all of the perfect numbers between 1 and 10,000.
 """
 
 
