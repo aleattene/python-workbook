5ec5b10e3cb1819f753e3f7a9c94c81068ad2ea0
Alessandro Attene <74595044+aleattene@users.noreply.github.com>
2021-10-28 16:22:59 +0200

    style: exe_143 - improved description

--- chap_06/exe_143_anagrams.py
+++ chap_06/exe_143_anagrams.py
@@ -1,6 +1,12 @@
 """
-The program receives TWO WORDS from the USER
-and returns (displaying it) whether or not THEY are ANAGRAMS.
+Two words are anagrams if they contain all of the same letters, but in a different order.
+
+For example:
+“evil” and “live” are anagrams because each contains one “e”, one “i”, one “l”, and one “v”.
+
+Create a program that reads two strings from the user,
+determines whether or not they are anagrams, and
+reports the result.
 """
 
 
