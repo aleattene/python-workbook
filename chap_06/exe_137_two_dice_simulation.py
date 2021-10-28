1e8c74793982c5dd8c89546c80589768436758a5
Alessandro Attene <74595044+aleattene@users.noreply.github.com>
2021-10-28 15:45:02 +0200

    style: exe_137 - improved description

--- chap_06/exe_137_two_dice_simulation.py
+++ chap_06/exe_137_two_dice_simulation.py
@@ -1,5 +1,14 @@
 """
-The program SIMULATES 1000 ROLLS of TWO DICE and displays:
+In this exercise you will simulate 1,000 rolls of two dice.
+Begin by writing a function that simulates rolling a pair of six-sided dice.
+Your function will not take any parameters.
+It will return the total that was rolled on two dice as its only result.
+Write a main program that uses your function to simulate rolling two six-sided dice 1,000 times.
+As your program runs, it should count the number of times that each total occurs.
+Then it should display a table that summarizes this data.
+Express the frequency for each total as a percentage of the number of rolls performed.
+Your program should also display the percentage expected by probability theory for each total.
+To summarize, the program should SIMULATES 1000 ROLLS of TWO DICE and displays:
 - the TOTAL that was OBTAINED with the two dice
 - the PERCENTAGE of the NUMBER of TIMES when one total was obtained
 - the percentage EXPECTED by PROBABILITY THEORY for each total obtained
