2f7c48a48447425fd00bd4c2823ad7758548e65a
Alessandro Attene <74595044+aleattene@users.noreply.github.com>
2021-10-28 16:33:41 +0200

    style: exe_145 - improved description

--- chap_06/exe_145_scrabble_score.py
+++ chap_06/exe_145_scrabble_score.py
@@ -1,6 +1,23 @@
 """
-The program receives a WORD from the USER
-and returns (displaying it) the SCRABBLE SCORE.
+In the game of Scrabble, each letter has points associated with it.
+The total score of a word is the sum of the scores of its letters.
+
+More common letters are worth fewer points while less common letters are worth more points.
+The points associated with each letter are shown below:
+
+    Points      	            Letters
+      1	                A, E, I, L, N, O, R, S, T, U
+2	D, G
+3	B, C, M, P
+4	F, H, V, W, Y
+5	K
+8	J, X
+10	Q, Z
+Write a program that computes and displays the Scrabble score for a word.
+
+Create a dictionary that maps from letters to point values. Then use the dictionary to compute the score.
+
+Documentation
 """
 
 
