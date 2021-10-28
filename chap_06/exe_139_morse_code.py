01102614ec89e3d0f69292760606d8d68c22eb03
Alessandro Attene <74595044+aleattene@users.noreply.github.com>
2021-10-28 16:04:37 +0200

    style: exe_139 - improved description

--- chap_06/exe_139_morse_code.py
+++ chap_06/exe_139_morse_code.py
@@ -1,7 +1,46 @@
 """
-The program receives a TEXT MESSAGE from the USER
-and returns (displaying it) the same message 
-ENCODED using MORSE CODE.
+Morse code is an encoding scheme that uses dashes and dots to represent digits and letters.
+In this exercise, you will write a program that uses a dictionary to store the mapping
+from these symbols to Morse code.
+
+Use a period to represent a dot, and a hyphen to represent a dash.
+The mapping from characters to dashes and dots is shown in the Tables below:
+
+    Character	        Code	            Character	        Code
+        A	            . _	                    N	             _ .
+        B	            _ . . .	                O	            _ _ _
+        C	            _ . _ .	                P	            . _ _ .
+        D	            _ . .	                Q	            _ _ . _
+        E	            .	                    R	            . _ .
+        F	            . . _ .	                S	            . . .
+        G	            _ _ .	                T	            _
+        H	            . . . .	                U	            . . _
+        I	            . .	                    V	            . . . _
+        J	            . _ _ _	                W	            . _ _
+        K	            _ . _	                X	            _ . . _
+        L	            . _ . .	                Y	            _ . _ _
+        M	            _ _	                    Z	            _ _ . .
+
+      Numbers	         Code	                 Numbers	            Code
+        1	            . _ _ _ _	                6	              _ . . . .
+        2	            . . _ _ _	                7	              _ _ . . .
+        3	            . . . _ _	                8	              _ _ _ . .
+        4	            . . . . _	                9	              _ _ _ _ .
+        5	            . . . . .	                0	              _ _ _ _ _
+
+    Character	        Code	                Character	            Code
+        Á	            . _ _ . _	                Ä	                . _ . _
+        É	            . . _ . .	                Ñ	                _ _ . _ _
+        Ö	            _ _ _ .	                    Ü	                . . _ _
+
+
+Your program should read a message from the user.
+Then it should translate all of the letters and digits in the message to Morse code,
+leaving a space between each sequence of dashes and dots.
+Your program should ignore any characters that are not listed in the previous table.
+
+The Morse code for Hello, World! is shown below:
+.... . .-.. .-.. --- .-- --- .-. .-.. -..
 """
 
 
