5614ac604a694b16761dfef7203f2141d00968cc
Alessandro Attene <74595044+aleattene@users.noreply.github.com>
2021-10-29 11:34:15 +0200

    style: exe_180 - improved description

--- chap_08/exe_180_string_edit_distance.py
+++ chap_08/exe_180_string_edit_distance.py
@@ -1,9 +1,35 @@
 """
-The program receives TWO WORDS from the USER 
-and return their EDIT DISTANCE, 
-considering that the EDIT DISTANCE is 
-the MINIMUM NUMBER of INSERT, DELETE and SUBSTITUTE OPERATIONS 
-needed to TRANSFORM ONE word into the OTHER.
+The edit distance between two strings is a measure of their similarity.
+
+The smaller the edit distance, the more similar the strings are with regard to the minimum number of insert,
+delete and substitute operations needed to transform one string into the other.
+Consider the strings kitten and sitting.
+
+The first string can be transformed into the second string with the following operations: Substitute the k with an s,
+substitute the e with an i, and insert a g at the end of the string.
+This is the smallest number of operations that can be performed to transform kitten into sitting.
+
+As a result, the edit distance is 3.
+
+Write a recursive function that computes the edit distance between two strings.
+
+Use the following algorithm:
+
+Let s and t be the strings
+    If the length of s is 0 then
+        Return the length of t
+    Else if the length of t is 0 then
+        Return the length of s
+    Else
+        Set cost to 0
+    If the last character in s does not equal the last character in t then
+        Set cost to 1
+        Set d1 equal to the edit distance between all characters except the last one
+            in s, and all characters in t, plus 1
+        Set d2 equal to the edit distance between all characters in s, and all
+            characters except the last one in t, plus 1
+        Set d3 equal to the edit distance between all characters except the last one
+            in s, and all characters except the last one in t, plus cost Return the minimum of d1, d2 and d3
 """
 
 
